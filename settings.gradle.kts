3e6dbb00da1a847833b72a80342dd9a0f437033c
Josh Muleshi <jmuleshi2@gmail.com>
2022-12-08 12:47:37 +0200

    add new modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,7 @@ pluginManagement {
         id("de.fayard.refreshVersions") version "0.51.0"
         id("com.android.application") version "7.2.2"
         id("org.jetbrains.kotlin.android") version "1.7.20"
+        id("com.android.library") version "7.3.1"
     }
 }
 plugins {
@@ -22,4 +23,12 @@ dependencyResolutionManagement {
 }
 rootProject.name = "Ecodim"
 include(":app")
-include(":buildsrc")
+include(":data")
+include(":domain")
+include(":util")
+include(":i18n")
+include(":ui-auth")
+include(":ui-common")
+include(":ui-home")
+include(":ui-navigation")
+include(":ui-onboarding")
