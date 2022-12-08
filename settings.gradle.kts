78082d87862d227e95326c9634090cb7a01071f1
Josh Muleshi <jmuleshi2@gmail.com>
2022-12-08 11:16:33 +0200

    set the basic buildScr module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,17 @@
-plugins {
-    id("de.fayard.refreshVersions") version "0.51.0"
-}
 pluginManagement {
     repositories {
         gradlePluginPortal()
         google()
         mavenCentral()
     }
+    plugins {
+        id("de.fayard.refreshVersions") version "0.51.0"
+        id("com.android.application") version "7.2.2"
+        id("org.jetbrains.kotlin.android") version "1.7.20"
+    }
+}
+plugins {
+    id("de.fayard.refreshVersions")
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
@@ -16,4 +21,5 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "Ecodim"
-include(':app')
+include(":app")
+include(":buildsrc")
