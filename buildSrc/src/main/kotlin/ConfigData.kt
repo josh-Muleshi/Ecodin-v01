61e5aada73d2d42052db47e11b948c84b436587f
Josh Muleshi <jmuleshi2@gmail.com>
2022-12-08 12:12:54 +0200

    all file commited

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -1,10 +1,10 @@
 object ConfigData {
-    const val compileSdk = 32
+    const val compileSdk = 33
     const val buildToolsVersion = "30.0.3"
 
     const val applicationId = "com.joshMuleshi.ecodim"
     const val minSdk = 21
-    const val targetSdk = 32
+    const val targetSdk = 33
     const val versionCode = 1
     const val versionName = "1.0"
     const val jvmTarget = "1.8"
