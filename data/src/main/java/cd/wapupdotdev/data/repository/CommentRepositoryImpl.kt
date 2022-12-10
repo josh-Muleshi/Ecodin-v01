0e53e6229e7ad83ecbd91fe1cde0c3175c68d500
Josh Muleshi <jmuleshi2@gmail.com>
2022-12-10 12:45:18 +0200

    comment repos impl done

--- data/src/main/java/cd/wapupdotdev/data/repository/CommentRepositoryImpl.kt
+++ data/src/main/java/cd/wapupdotdev/data/repository/CommentRepositoryImpl.kt
@@ -1,2 +1,20 @@
 package cd.wapupdotdev.data.repository
 
+import cd.wapupdotdev.data.datasource.comment.CommentDataSource
+import cd.wapupdotdev.domain.entity.Comment
+import cd.wapupdotdev.domain.repository.CommentRepository
+import cd.wapupdotdev.util.Result
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CommentRepositoryImpl @Inject constructor(
+    private val dataSource: CommentDataSource
+): CommentRepository {
+    override fun fetchAllComment(): Flow<Result<List<Comment?>>> {
+        return dataSource.getAllComment()
+    }
+
+    override suspend fun addComment(comment: Comment) {
+        return dataSource.addComment(comment)
+    }
+}
