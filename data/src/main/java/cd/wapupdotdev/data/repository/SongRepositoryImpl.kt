801a818a7000817fbeb8a31570c9bd3653b5197c
Josh Muleshi <jmuleshi2@gmail.com>
2022-12-10 12:49:24 +0200

    song repos impl done

--- data/src/main/java/cd/wapupdotdev/data/repository/SongRepositoryImpl.kt
+++ data/src/main/java/cd/wapupdotdev/data/repository/SongRepositoryImpl.kt
@@ -1,4 +1,16 @@
 package cd.wapupdotdev.data.repository
 
-class SongRepositoryImpl {
+import cd.wapupdotdev.data.datasource.song.SongDataSource
+import cd.wapupdotdev.domain.entity.Song
+import cd.wapupdotdev.domain.repository.SongRepository
+import cd.wapupdotdev.util.Result
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SongRepositoryImpl @Inject constructor(
+    private val dataSource: SongDataSource
+): SongRepository{
+    override fun findSong(): Flow<Result<Song?>> {
+        return dataSource.findSong()
+    }
 }
