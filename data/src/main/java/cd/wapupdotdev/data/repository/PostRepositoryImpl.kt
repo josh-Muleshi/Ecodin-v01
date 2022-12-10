eccb229c09f5c1b9dc34e1884bb691168af0ab47
Josh Muleshi <jmuleshi2@gmail.com>
2022-12-10 12:47:41 +0200

    post repos impl done

--- data/src/main/java/cd/wapupdotdev/data/repository/PostRepositoryImpl.kt
+++ data/src/main/java/cd/wapupdotdev/data/repository/PostRepositoryImpl.kt
@@ -1,2 +1,20 @@
 package cd.wapupdotdev.data.repository
 
+import cd.wapupdotdev.data.datasource.post.PostDataSource
+import cd.wapupdotdev.domain.entity.Post
+import cd.wapupdotdev.domain.repository.PostRepository
+import cd.wapupdotdev.util.Result
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class PostRepositoryImpl @Inject constructor(
+    private val dataSource: PostDataSource
+): PostRepository {
+    override fun fetchAllPost(): Flow<Result<List<Post>>> {
+        return dataSource.fetchAllPost()
+    }
+
+    override fun findPostByUid(uidPost: String): Flow<Result<Post>> {
+        return dataSource.findPostByUid(uidPost)
+    }
+}
