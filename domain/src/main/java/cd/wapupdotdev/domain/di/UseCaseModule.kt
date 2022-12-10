3250405c5af16bb9c57089dac44436423c29ebcf
Josh Muleshi <jmuleshi2@gmail.com>
2022-12-10 10:52:31 +0200

    provided all use cases

--- domain/src/main/java/cd/wapupdotdev/domain/di/UseCaseModule.kt
+++ domain/src/main/java/cd/wapupdotdev/domain/di/UseCaseModule.kt
@@ -1,6 +1,7 @@
 package cd.wapupdotdev.domain.di
 
-import cd.wapupdotdev.domain.repository.SongRepository
+import cd.wapupdotdev.domain.repository.*
+import cd.wapupdotdev.domain.usecase.*
 import cd.wapupdotdev.util.IoDispatcher
 import dagger.Module
 import dagger.Provides
@@ -18,7 +19,169 @@ object UseCaseModule {
     fun provideSongUseCase(
         @IoDispatcher dispatcher: CoroutineDispatcher,
         repository: SongRepository
-    ){
+    ): FindSongUseCase {
+        return FindSongUseCase(dispatcher, repository)
+    }
 
+    @Provides
+    @Singleton
+    fun provideVerseUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: VerseRepository
+    ): FindVerseUseCase{
+        return FindVerseUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCommentUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: CommentRepository
+    ): FetchAllCommentUseCase {
+        return FetchAllCommentUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAddCommentUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: CommentRepository
+    ): AddCommentUseCase{
+        return AddCommentUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFindCurrentUserUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: UserRepository
+    ): FindCurrentUserUseCase {
+        return FindCurrentUserUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFindCurrentUserAsyncUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: UserRepository
+    ): FindCurrentUserAsyncUseCase {
+        return FindCurrentUserAsyncUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSignInWithEmailAndPasswordUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: UserRepository
+    ): SignInWithEmailAndPasswordUseCase {
+        return SignInWithEmailAndPasswordUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideResentVerificationEmailUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: UserRepository
+    ): ResentVerificationEmailUseCase {
+        return ResentVerificationEmailUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSignUpWithEmailAndPasswordUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: UserRepository
+    ): SignUpWithEmailAndPasswordUseCase {
+        return SignUpWithEmailAndPasswordUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSignInWithEmailAndLinkUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: UserRepository
+    ): SignInWithEmailAndLinkUseCase {
+        return SignInWithEmailAndLinkUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSendSignInLinkToEmail(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: UserRepository
+    ): SendSignInLinkToEmailUseCase {
+        return SendSignInLinkToEmailUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSignInWithAuthCredentialUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: UserRepository
+    ): SignInWithAuthCredentialUseCase {
+        return SignInWithAuthCredentialUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideSaveUserInfoUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: UserRepository
+    ): SaveUserInfoUseCase {
+        return SaveUserInfoUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFetchAllPostUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: PostRepository
+    ): FetchAllPostUseCase {
+        return FetchAllPostUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFindPostByUidUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: PostRepository
+    ): FindPostByUidUseCase {
+        return FindPostByUidUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFindAllNotificationUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: NotificationRepository
+    ): FindOrderNotificationsUseCase {
+        return FindOrderNotificationsUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFindGlobalNotificationsUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: NotificationRepository
+    ): FindGlobalNotificationsUseCase {
+        return FindGlobalNotificationsUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFindUnSeenNotificationUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: NotificationRepository
+    ): FindOrderUnSeenNotificationUseCase {
+        return FindOrderUnSeenNotificationUseCase(dispatcher, repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMarkNotificationAsSeenUseCase(
+        @IoDispatcher dispatcher: CoroutineDispatcher,
+        repository: NotificationRepository
+    ): MarkOrderNotificationAsSeenUseCase {
+        return MarkOrderNotificationAsSeenUseCase(dispatcher, repository)
     }
 }
